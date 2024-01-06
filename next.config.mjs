4b2eead499e12232184184122fee1c66fbe2c129
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-01-06 01:18:58 +0000

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+import million from 'million/compiler';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '*',
+				port: '',
+			},
+		],
+	},
+	env: {
+		API_URL: process.env.API_URL,
+	},
+};
+
+let configExport = nextConfig;
+
+if (process.env.NODE_ENV === 'production') {
+	console.log('Loaded production config');
+	configExport = million.next(nextConfig, { auto: true });
+}
+
+export default configExport;
