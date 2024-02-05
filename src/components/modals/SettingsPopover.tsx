a83919f70a1c84cff11b94cd564b4e60dca14466
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-02-05 13:26:27 +0000

    settings popover instead of modal

---
+++ src/components/modals/SettingsPopover.tsx
@@ -0,0 +1,177 @@
+import type { ReactElement } from 'react';
+import useTheme from '@/hooks/useTheme';
+import {
+	Box,
+	Button,
+	Flex,
+	FormControl,
+	FormLabel,
+	Heading,
+	Icon,
+	Popover,
+	PopoverBody,
+	PopoverCloseButton,
+	PopoverContent,
+	PopoverHeader,
+	PopoverTrigger,
+	Portal,
+	SimpleGrid,
+	SlideFade,
+	Spacer,
+	Stack,
+	Text,
+	useDisclosure
+} from '@chakra-ui/react';
+import SelectModal from './SelectModal';
+import useLanguage from '@/hooks/useLanguage';
+import useThemeValues from '@/hooks/useThemeValues';
+import useLanguageStore from '@/store/language';
+import { MdCheckCircle, MdFlag, MdKeyboardArrowDown } from 'react-icons/md';
+
+interface SettingPopoverProps {
+	trigger: ReactElement;
+}
+
+const SettingsPopover = ({ trigger }: SettingPopoverProps): ReactElement => {
+	const { getThemeValue } = useThemeValues();
+	const [languageId, languages] = useLanguage();
+	const { setLanguageId } = useLanguageStore();
+	const [themeId, setThemeId, themes] = useTheme();
+	const { isOpen: isLangOpen, onClose: onLangClose, onOpen: onLangOpen } = useDisclosure();
+
+	return (
+		<>
+			<SelectModal
+				isOpen={isLangOpen}
+				onClose={onLangClose}
+				listItems={languages.map(({ id, name, extension }) => ({
+					id,
+					name,
+					details: languageId === id ? 'Recently used' : 'Set language',
+					icon: <MdFlag />,
+					alias: extension ? [extension] : undefined
+				}))}
+				initialSelectedId={languageId}
+				onPreview={setLanguageId}
+				onSelect={setLanguageId}
+			/>
+			<Popover isLazy>
+				<PopoverTrigger>{trigger}</PopoverTrigger>
+				<Portal>
+					<PopoverContent
+						width={['90vw', '430px']}
+						bg={getThemeValue('popup')}
+						margin='5px'
+						mr='20px'
+					>
+						<PopoverHeader>Settings</PopoverHeader>
+						<PopoverCloseButton />
+						<PopoverBody>
+							<Stack spacing='15px'>
+								<FormControl gap='5px' display='flex' flexDirection='column'>
+									<FormLabel htmlFor='language'>
+										<Heading size='sm'>Language</Heading>
+									</FormLabel>
+									<Button
+										id='language'
+										rightIcon={<MdKeyboardArrowDown />}
+										onClick={onLangOpen}
+									>
+										{languages.find((lang) => lang.id === languageId)?.name ??
+											languages[0]?.name}
+									</Button>
+								</FormControl>
+								<FormControl gap='5px' display='flex' flexDirection='column'>
+									<FormLabel htmlFor='theme'>
+										<Heading size='sm'>Theme selector</Heading>
+									</FormLabel>
+									<SimpleGrid
+										gap='20px'
+										id='theme'
+										minChildWidth={['100px', '85px']}
+									>
+										{themes.map((theme, i) => (
+											<Box
+												key={theme.id}
+												w='100%'
+												h={['70px', '70px']}
+												bg={theme.values.primaryDisplay}
+												borderRadius='10px'
+												style={
+													theme.id === themeId
+														? {
+																outline: '3px solid',
+																outlineOffset: '-3px',
+																outlineColor:
+																	theme.values.midTransparency
+															}
+														: undefined
+												}
+												_hover={{
+													outline: '3px solid',
+													outlineOffset: '-3px',
+													outlineColor: theme.values.lowTransparency
+												}}
+												onClick={() => setThemeId(theme.id)}
+											>
+												<Flex
+													h='100%'
+													w='100%'
+													gap='5px'
+													direction='column'
+													alignItems='center'
+												>
+													<Spacer />
+													<Box
+														w='100%'
+														borderBottomRadius='10px'
+														bg={theme.values.lowAltTransparency}
+													>
+														<Flex
+															w='100%'
+															gap='2px'
+															p='5px'
+															direction='row'
+															alignItems='center'
+														>
+															<Text
+																paddingX='5px'
+																fontSize='12px'
+																color={theme.values.text}
+																noOfLines={1}
+															>
+																{theme.name}
+															</Text>
+															<Spacer />
+															<SlideFade
+																in={
+																	theme.id === themeId ||
+																	(!themeId && i === 0)
+																}
+															>
+																{(theme.id === themeId ||
+																	(!themeId && i === 0)) && (
+																	<Icon
+																		as={MdCheckCircle}
+																		zIndex={40}
+																		fontSize={['15px', '15px']}
+																	/>
+																)}
+															</SlideFade>
+														</Flex>
+													</Box>
+												</Flex>
+											</Box>
+										))}
+									</SimpleGrid>
+								</FormControl>
+							</Stack>
+						</PopoverBody>
+					</PopoverContent>
+				</Portal>
+			</Popover>
+		</>
+	);
+};
+
+export default SettingsPopover;
