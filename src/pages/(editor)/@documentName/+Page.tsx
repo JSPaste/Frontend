5aacd8a032d3c3cc65b6b9988cb4524db90d76b3
Ivan Gabaldon <git@inetol.net>
2024-09-09 17:34:14 +0200

    cleanup

--- src/pages/(editor)/@documentName/+Page.tsx
+++ src/pages/(editor)/@documentName/+Page.tsx
@@ -2,6 +2,7 @@ import EditorScreen from '@x-component/screens/Editor';
 import type { Data } from '@x-page/(editor)/@documentName/+data.ts';
 import { useData } from 'vike-react/useData';
 
+// https://github.com/vikejs/vike/issues/1476
 export default function () {
 	const data = useData<Data>();
 
