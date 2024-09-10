6d1e4c7aac0d723c8fdf939d6bea60daafc32760
Ivan Gabaldon <git@inetol.net>
2024-09-10 15:36:24 +0200

    Initial update

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,12 +1,25 @@
-import { tags as t } from '@lezer/highlight';
-import { hyperLink } from '@uiw/codemirror-extensions-hyper-link';
-import { vscodeDark, vscodeLight } from '@uiw/codemirror-theme-vscode';
-import createTheme from '@uiw/codemirror-themes';
-import ReactCodeMirror, { type Extension, type ReactCodeMirrorRef } from '@uiw/react-codemirror';
+import { closeBrackets, closeBracketsKeymap } from '@codemirror/autocomplete';
+import { defaultKeymap, history, historyKeymap } from '@codemirror/commands';
+import { bracketMatching, defaultHighlightStyle, indentOnInput, syntaxHighlighting } from '@codemirror/language';
+import { EditorState } from '@codemirror/state';
+import {
+	EditorView,
+	crosshairCursor,
+	drawSelection,
+	dropCursor,
+	highlightActiveLine,
+	highlightActiveLineGutter,
+	highlightSpecialChars,
+	keymap,
+	lineNumbers,
+	placeholder,
+	rectangularSelection
+} from '@codemirror/view';
+import { hyperLinkExtension, hyperLinkStyle } from '@uiw/codemirror-extensions-hyper-link';
 import type { HeaderProps } from '@x-component/Header';
+import { editorThemes } from '@x-util/editorThemes.ts';
 import { languageStore, themeStore } from '@x-util/store';
-import type { ThemeKeys } from '@x-util/themes.ts';
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 type EditorProps = {
 	setCursorLocation: (info: HeaderProps) => void;
@@ -18,14 +31,14 @@ type EditorProps = {
 };
 
 export default function ({ setCursorLocation, setValue, value, isEditing, enableEdit }: EditorProps) {
-	const editorRef = useRef<ReactCodeMirrorRef>(null);
+	const container = useRef<HTMLDivElement>(null);
+	const view = useRef<EditorView>();
+	const [initialValue] = useState(value);
 
 	const updateCursorInformation = useCallback(() => {
-		const editor = editorRef.current;
-
-		if (editor?.view) {
-			const { from } = editor.view.state.selection.main;
-			const cursorPosition = editor.view.state.doc.lineAt(from);
+		if (view.current) {
+			const { from } = view.current.state.selection.main;
+			const cursorPosition = view.current.state.doc.lineAt(from);
 
 			setCursorLocation({
 				lineNumber: cursorPosition.number,
@@ -35,90 +48,65 @@ export default function ({ setCursorLocation, setValue, value, isEditing, enable
 	}, [setCursorLocation]);
 
 	const { getLanguage } = languageStore();
-
-	const onChange = useCallback(
-		(value: string) => {
-			updateCursorInformation();
-			setValue(value);
-		},
-		[updateCursorInformation, setValue]
-	);
-
 	const { themeId } = themeStore();
 
-	return (
-		<ReactCodeMirror
-			ref={editorRef}
-			onChange={onChange}
-			height='100%'
-			className='flex-grow overflow-auto'
-			extensions={[getLanguage(), hyperLink]}
-			theme={editorThemes[themeId]}
-			placeholder="Start writing here! When you're done, hit the save button to generate a unique URL with your content."
-			value={value}
-			readOnly={enableEdit && !isEditing}
-			autoFocus={true}
-			basicSetup={{
-				lineNumbers: true,
-				highlightActiveLineGutter: true,
-				highlightActiveLine: true,
-				autocompletion: true,
-				allowMultipleSelections: false,
-				syntaxHighlighting: true
-			}}
-		/>
-	);
-}
+	useEffect(() => {
+		if (!container.current) return;
 
-const editorThemes: Record<ThemeKeys, 'dark' | 'light' | Extension> = {
-	default: createTheme({
-		settings: {
-			background: '#202020',
-			foreground: '#CDCDCD',
-			caret: '#FFE184',
-			selection: '#FFE18419',
-			selectionMatch: '#FFE18433',
-			gutterBackground: '#1A1A1A',
-			gutterForeground: '#838383',
-			gutterActiveForeground: '#CDCDCD',
-			lineHighlight: '#FFE18407'
-		},
-		styles: [
-			{
-				tag: [t.comment],
-				color: '#c8c5bb'
-			},
-			{
-				tag: [t.operator],
-				color: '#e8b000'
-			},
-			{
-				tag: [t.unit, t.punctuation],
-				color: '#c19200'
-			},
-			{
-				tag: [t.propertyName],
-				color: '#fcbe00'
-			},
-			{
-				tag: [t.bracket, t.variableName, t.emphasis, t.heading, t.tagName, t.className, t.namespace],
-				color: '#dee2e6'
-			},
-			{
-				tag: [t.typeName, t.atom, t.number, t.keyword, t.link, t.attributeName, t.quote],
-				color: '#FFE184'
-			},
-			{
-				tag: [t.number],
-				color: '#84b6ff'
-			},
-			{
-				tag: [t.string, t.url],
-				color: '#84ffb0'
-			}
-		],
-		theme: 'dark'
-	}),
-	dark: vscodeDark,
-	light: vscodeLight
-};
+		view.current = new EditorView({
+			parent: container.current,
+			state: EditorState.create({
+				doc: initialValue,
+				extensions: [
+					placeholder(
+						"Start writing here! When you're done, hit the save button to generate a unique URL with your content."
+					),
+					lineNumbers(),
+					highlightActiveLineGutter(),
+					highlightSpecialChars(),
+					history(),
+					drawSelection(),
+					dropCursor(),
+					indentOnInput(),
+					syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+					bracketMatching(),
+					closeBrackets(),
+					rectangularSelection(),
+					crosshairCursor(),
+					highlightActiveLine(),
+					keymap.of([...closeBracketsKeymap, ...defaultKeymap, ...historyKeymap]),
+					editorThemes[themeId],
+					getLanguage(),
+					hyperLinkExtension(),
+					hyperLinkStyle,
+					EditorState.readOnly.of(enableEdit && !isEditing),
+					EditorView.theme({
+						'&': {
+							height: '100%'
+						},
+						'& .cm-scroller': {
+							height: '100% !important'
+						},
+						'& .cm-lineNumbers .cm-gutterElement': {
+							padding: '0 5px'
+						}
+					}),
+					EditorView.contentAttributes.of({ 'data-lt-active': 'false' }),
+					EditorView.updateListener.of((vu) => {
+						if (vu.docChanged) {
+							updateCursorInformation();
+							setValue(vu.state.doc.toString());
+						}
+					})
+				]
+			})
+		});
+
+		return () => {
+			view.current?.destroy();
+			view.current = undefined;
+		};
+	}, [themeId, getLanguage, initialValue, enableEdit, isEditing, setValue, updateCursorInformation]);
+
+	return <div ref={container} className='flex-grow overflow-hidden' />;
+}
