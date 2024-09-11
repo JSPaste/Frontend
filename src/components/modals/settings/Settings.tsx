44994e4896e1840893842fc6b052a2e674779f4b
Ivan Gabaldon <git@inetol.net>
2024-09-11 19:10:26 +0200

    improvements

--- src/components/modals/settings/Settings.tsx
+++ src/components/modals/settings/Settings.tsx
@@ -1,20 +1,23 @@
 import LanguageSection from '@x-component/modals/settings/LanguageSection';
 import ThemeSection from '@x-component/modals/settings/ThemeSection';
+import { Portal } from 'solid-js/web';
 
 export default function Settings() {
 	return (
-		<dialog id='modal_settings' class='modal modal-bottom sm:modal-middle'>
-			<div class='modal-box flex flex-col bg-base-100'>
-				<h3 class='font-bold text-lg'>SETTINGS</h3>
-				<div class='divider m-0.5' />
-				<div class='flex flex-col gap-4'>
-					<ThemeSection />
-					<LanguageSection />
+		<Portal>
+			<dialog id='modal_settings' class='modal modal-bottom sm:modal-middle'>
+				<div class='modal-box flex flex-col bg-base-100'>
+					<h3 class='font-bold text-lg'>SETTINGS</h3>
+					<div class='divider m-0.5' />
+					<div class='flex flex-col gap-4'>
+						<ThemeSection />
+						<LanguageSection />
+					</div>
 				</div>
-			</div>
-			<form method='dialog' class='modal-backdrop'>
-				<button type='submit' class='cursor-default' />
-			</form>
-		</dialog>
+				<form method='dialog' class='modal-backdrop'>
+					<button type='submit' class='cursor-default' />
+				</form>
+			</dialog>
+		</Portal>
 	);
 }
