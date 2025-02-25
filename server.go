d24c8d4a6e9130e66290e3184a1f98b6cbc9a091
Ivan Gabaldon <igabaldon@inetol.net>
2025-02-25 23:42:41 +0100

    Initial update

--- server.go
+++ server.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
 	"github.com/jspaste/frontend/www"
 	"github.com/valyala/fasthttp"
 	"io"
 	"log"
+	"log/slog"
 	"os"
-	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-	_ = godotenv.Load()
-
 	bindAddressEnv := getEnv("JSPF_BIND_ADDRESS", "localhost").(string)
 	portEnv := getEnv("JSPF_PORT", uint16(3000)).(uint16)
 
@@ -28,10 +26,11 @@ func main() {
 	requestHandler := fs.NewRequestHandler()
 
 	handler := func(ctx *fasthttp.RequestCtx) {
-		path := string(ctx.Path())
+		path := string(ctx.Request.URI().Path())
 
-		if path == "/" {
+		if !strings.Contains(path, ".") {
 			ctx.Request.SetRequestURI("/index.html")
+			ctx.Response.Header.Set("Content-Type", "text/html; charset=utf-8")
 		}
 
 		requestHandler(ctx)
@@ -42,16 +41,12 @@ func main() {
 			requestHandler(ctx)
 		}
 
-		if path == "/404" {
-			ctx.Response.SetStatusCode(fasthttp.StatusNotFound)
-		}
-
-		path = string(ctx.Path())
+		path = string(ctx.Request.URI().Path())
 
 		if strings.HasPrefix(path, "/assets/") {
-			ctx.Response.Header.Set("Cache-Control", "max-age=31536000, public, immutable")
+			ctx.Response.Header.Set("Cache-Control", "max-age=31536000, public, immutable, no-transform")
 		} else if strings.HasSuffix(path, ".html") {
-			ctx.Response.Header.Set("Cache-Control", "max-age=0, no-store")
+			ctx.Response.Header.Set("Cache-Control", "max-age=0, public, must-revalidate")
 		} else {
 			ctx.Response.Header.Set("Cache-Control", "max-age=600, public, no-transform")
 		}
@@ -60,79 +55,18 @@ func main() {
 	}
 
 	server := &fasthttp.Server{
-		Logger:                log.New(io.Discard, "", 0),
-		Handler:               handler,
 		GetOnly:               true,
+		Handler:               handler,
+		Logger:                log.New(io.Discard, "", 0),
 		NoDefaultServerHeader: true,
+		ReadTimeout:           60 * time.Second,
+		WriteTimeout:          60 * time.Second,
 	}
 
-	log.Print("Listening on http://", bindAddressEnv, ":", portEnv, "\n")
+	slog.Info("Server running;", "bindAddress", bindAddressEnv, "port", portEnv)
 
-	log.Fatal(server.ListenAndServe(fmt.Sprint(bindAddressEnv, ":", portEnv)))
-}
-
-func getEnv(key string, defaultValue interface{}) interface{} {
-	if value, exists := os.LookupEnv(key); exists {
-		switch defaultValue.(type) {
-		case int8:
-			if intValue, err := strconv.ParseInt(value, 10, 8); err == nil {
-				return int8(intValue)
-			}
-		case int16:
-			if intValue, err := strconv.ParseInt(value, 10, 16); err == nil {
-				return int16(intValue)
-			}
-		case int32:
-			if intValue, err := strconv.ParseInt(value, 10, 32); err == nil {
-				return int32(intValue)
-			}
-		case int64:
-			if intValue, err := strconv.ParseInt(value, 10, 64); err == nil {
-				return int(intValue)
-			}
-		case int:
-			if intValue, err := strconv.Atoi(value); err == nil {
-				return intValue
-			}
-		case uint8:
-			if uintValue, err := strconv.ParseUint(value, 10, 8); err == nil {
-				return uint8(uintValue)
-			}
-		case uint16:
-			if uintValue, err := strconv.ParseUint(value, 10, 16); err == nil {
-				return uint16(uintValue)
-			}
-		case uint32:
-			if uintValue, err := strconv.ParseUint(value, 10, 32); err == nil {
-				return uint32(uintValue)
-			}
-		case uint64:
-			if uintValue, err := strconv.ParseUint(value, 10, 64); err == nil {
-				return uintValue
-			}
-		case uint:
-			if uintValue, err := strconv.ParseUint(value, 10, 64); err == nil {
-				return uint(uintValue)
-			}
-		case float32:
-			if floatValue, err := strconv.ParseFloat(value, 32); err == nil {
-				return float32(floatValue)
-			}
-		case float64:
-			if floatValue, err := strconv.ParseFloat(value, 64); err == nil {
-				return floatValue
-			}
-		case bool:
-			if boolValue, err := strconv.ParseBool(value); err == nil {
-				return boolValue
-			}
-		case string:
-			return value
-		}
-
-		log.Printf("Unexpected value for env \"%s\": got \"%s\", falling back to default...", key, value)
-		return defaultValue
+	if err := server.ListenAndServe(fmt.Sprintf("%s:%d", bindAddressEnv, portEnv)); err != nil {
+		slog.Error("Unexpected server status;", "error", err)
+		os.Exit(1)
 	}
-
-	return defaultValue
 }
