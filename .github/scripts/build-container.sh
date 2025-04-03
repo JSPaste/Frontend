ecc4e1e97d1fca3d701df64f52e46e313efb8a40
Ivan Gabaldon <igabaldon@inetol.net>
2025-04-03 12:32:44 +0200

    manual builds

---
+++ .github/scripts/build-container.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Target platforms
+platforms=("linux/amd64" "linux/arm64")
+
+timestamp="$(date +%Y.%m.%d)"
+timestamp_iso="$(date -u +%Y-%m-%dT%H:%M:%SZ)"
+version="${timestamp}-${GHA_SHA_SHORT}"
+
+# Tags
+if [[ "${GHA_BRANCH}" == "stable" ]]; then
+    tags+=("latest")
+else
+    tags+=("snapshot")
+fi
+
+tags+=("${GHA_SHA}")
+tags+=("${version}")
+
+# Build container images
+for platform in "${platforms[@]}"; do
+    arch=$(echo "$platform" | cut --delimiter='/' --fields=2)
+
+    buildah build --platform "$platform" --format=oci --layers=true --squash --identity-label=false \
+        --label=org.opencontainers.image.created="$timestamp_iso" \
+        --label=org.opencontainers.image.revision="$GHA_SHA" \
+        --label=org.opencontainers.image.version="$version" \
+        -f ./Dockerfile \
+        -t "$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$arch" .
+done
+
+# Create manifests
+for tag in "${tags[@]}"; do
+    buildah manifest rm "$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag" 2>/dev/null || true
+    buildah manifest create "$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag"
+
+    for platform in "${platforms[@]}"; do
+        arch=$(echo "$platform" | cut --delimiter='/' --fields=2)
+
+        buildah manifest add "$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag" "$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$arch"
+    done
+done
+
+# If running locally, we are done
+set +u
+if [[ -z "$GITHUB_OUTPUT" ]]; then
+    exit 0
+fi
+set -u
+
+# If running in GHA, set output variables
+echo "tags=${tags[*]}" >>"$GITHUB_OUTPUT"
---
+++ .github/scripts/release-container.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+IFS=' ' read -ra registries <<<"$GHA_REGISTRY"
+IFS=' ' read -ra registry_accounts <<<"$GHA_REGISTRY_ACCOUNT_NAME"
+IFS=' ' read -ra registry_tokens <<<"$GHA_REGISTRY_ACCOUNT_TOKEN"
+
+if [[ "${#registries[@]}" != "${#registry_accounts[@]}" || "${#registries[@]}" != "${#registry_tokens[@]}" ]]; then
+    echo "Number of registries, accounts and tokens must match; (\"${#registries[@]}\" != \"${#registry_accounts[@]}\" != \"${#registry_tokens[@]}\"). Exiting..."
+    exit 1
+fi
+
+IFS=' ' read -ra tags <<<"$GHA_TAG"
+
+# Release container images
+for i in "${!registries[@]}"; do
+    registry="${registries[$i]}"
+    account="${registry_accounts[$i]}"
+    token="${registry_tokens[$i]}"
+
+    buildah login --username "$account" --password-stdin "$registry" <<<"$token"
+
+    for tag in "${tags[@]}"; do
+        buildah manifest push --digestfile "./$GHA_CONTAINER_ORGANIZATION-$GHA_CONTAINER_IMAGE-${tag}_digest.txt" \
+            "localhost/$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag" "$registry/$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag"
+
+        digest="$(cat "./$GHA_CONTAINER_ORGANIZATION-$GHA_CONTAINER_IMAGE-${tag}_${registry}_digest.txt")"
+        digest_cmp="$(cat "./$GHA_CONTAINER_ORGANIZATION-$GHA_CONTAINER_IMAGE-${tags[0]}_${registry}_digest.txt")"
+
+        # digests should be the same independent of the registry/tags used but just in case
+        if [[ "$digest" != "$digest_cmp" ]]; then
+            echo "Digests do not match for $tag; (\"$digest\" != \"$digest_cmp\"). Exiting..."
+            exit 1
+        fi
+    done
+done
+
+# If running locally, we are done
+set +u
+if [[ -z "$GITHUB_OUTPUT" ]]; then
+    exit 0
+fi
+set -u
+
+# If running in GHA, set output variables
+echo "digest=$(cat "./$GHA_CONTAINER_ORGANIZATION-$GHA_CONTAINER_IMAGE-${tags[0]}_${registry}_digest.txt")" >>"$GITHUB_OUTPUT"
+echo "registries=[$(printf '"%s",' "${registries[@]}" | sed 's/,$//')]" >>"$GITHUB_OUTPUT"
