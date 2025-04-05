5e60d1ca4eb965ea800e86685ce82f46942a5532
Ivan Gabaldon <git@inetol.net>
2025-04-05 23:47:22 +0200

    Add quay.io registry (#274)

---
+++ .github/scripts/build-container.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Target platforms
+platforms=("linux/amd64" "linux/arm64")
+
+timestamp="$(date +%Y.%m.%d)"
+timestamp_iso="$(date -u +%Y-%m-%dT%H:%M:%SZ)"
+version="${timestamp}-${GHA_SHA_SHORT}"
+
+# Tags
+if [[ "${GHA_BRANCH}" == "stable" ]]; then
+    tags+=("latest")
+else
+    tags+=("snapshot")
+fi
+
+tags+=("${GHA_SHA}")
+tags+=("${version}")
+
+# Build container images
+for platform in "${platforms[@]}"; do
+    arch=$(echo "$platform" | cut --delimiter='/' --fields=2)
+
+    podman build --platform "$platform" --format=oci --layers --identity-label=false \
+        --label=org.opencontainers.image.created="$timestamp_iso" \
+        --label=org.opencontainers.image.revision="$GHA_SHA" \
+        --label=org.opencontainers.image.version="$version" \
+        -f ./Dockerfile \
+        -t "$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$arch-pure" \
+        .
+
+    podman build --platform "$platform" --format=oci --layers --squash-all --identity-label=false \
+        --label=org.opencontainers.image.created="$timestamp_iso" \
+        --label=org.opencontainers.image.revision="$GHA_SHA" \
+        --label=org.opencontainers.image.version="$version" \
+        -f ./Dockerfile \
+        -t "$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$arch" \
+        .
+done
+
+# Create manifests
+for tag in "${tags[@]}"; do
+    # TODO: Replace error redir with "--ignore" on podman v5?
+    podman manifest rm "localhost/$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag" 2>/dev/null || true
+    podman manifest create "localhost/$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag"
+
+    for platform in "${platforms[@]}"; do
+        arch=$(echo "$platform" | cut --delimiter='/' --fields=2)
+
+        podman manifest add \
+            "localhost/$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag" \
+            "containers-storage:localhost/$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$arch"
+    done
+done
+
+# If running locally, we are done
+set +u
+if [[ -z "$GITHUB_OUTPUT" ]]; then
+    exit 0
+fi
+set -u
+
+# If running in GHA, set output variables
+echo "tags=${tags[*]}" >>"$GITHUB_OUTPUT"
---
+++ .github/scripts/release-container.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+registries=()
+IFS=' ' read -ra registries <<<"$GHA_REGISTRY"
+
+registry_accounts=()
+IFS=' ' read -ra registry_accounts <<<"$GHA_REGISTRY_ACCOUNT_NAME"
+
+registry_tokens=()
+IFS=' ' read -ra registry_tokens <<<"$GHA_REGISTRY_ACCOUNT_TOKEN"
+
+# shellcheck disable=SC2055
+if [[ "${#registries[@]}" != "${#registry_accounts[@]}" || "${#registries[@]}" != "${#registry_tokens[@]}" ]]; then
+    echo "Number of registries, accounts and tokens must match; (GHA_REGISTRY=\"${#registries[@]}\" != GHA_REGISTRY_ACCOUNT_NAME=\"${#registry_accounts[@]}\" != GHA_REGISTRY_ACCOUNT_TOKEN=\"${#registry_tokens[@]}\"). Exiting..."
+    exit 1
+fi
+
+tags=()
+IFS=' ' read -ra tags <<<"$GHA_TAG"
+
+for tag in "${tags[@]}"; do
+    podman manifest exists "localhost/$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag"
+done
+
+# Release container images
+for i in "${!registries[@]}"; do
+    registry="${registries[$i]}"
+    account="${registry_accounts[$i]}"
+    token="${registry_tokens[$i]}"
+
+    podman login --username "$account" --password-stdin "$registry" <<<"$token"
+
+    for tag in "${tags[@]}"; do
+        podman manifest push --all --tls-verify --digestfile=./container-image_digest.txt \
+            "localhost/$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag" \
+            "docker://$registry/$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag"
+    done
+
+    podman logout --all
+done
+
+# Cleanup
+for tag in "${tags[@]}"; do
+    # TODO: Replace error redir with "--ignore" on podman v5?
+    podman manifest rm "localhost/$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE:$tag" 2>/dev/null || true
+done
+
+for id in $(podman images --format="{{.ID}}" --filter=reference="localhost/$GHA_CONTAINER_ORGANIZATION/$GHA_CONTAINER_IMAGE*"); do
+    # TODO: Replace error redir with "--ignore" on podman v5?
+    podman rmi "$id" 2>/dev/null || true
+done
+
+podman image list
+
+# If running locally, we are done
+set +u
+if [[ -z "$GITHUB_OUTPUT" ]]; then
+    exit 0
+fi
+set -u
+
+# If running in GHA, set output variables
+echo "digest=$(cat ./container-image_digest.txt)" >>"$GITHUB_OUTPUT"
+echo "registries=[$(printf '"%s",' "${registries[@]}" | sed 's/,$//')]" >>"$GITHUB_OUTPUT"
